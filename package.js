c867b0e487e6e0221cc8f6cc4db24e1f82df57a4
Brent Abrahams <brent_abrahams@yahoo.com>
2014-12-30 12:52:30 +1300

    Fix for wysiwyg/textarea conflict

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:editable-text",
   summary: "Editable text drop-in template helper",
-  version: "0.5.4",
+  version: "0.5.5",
   git: "https://github.com/jackadams/meteor-editable-text.git"
 });
 
