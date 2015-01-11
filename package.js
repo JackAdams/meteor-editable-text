2b9b6d1aa17e84ac6becfccac8452de9c27e7b7f
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-11 17:34:48 +0800

    Fixes the bug if useTransactions is true without the transactions package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:editable-text",
   summary: "Editable text drop-in template helper",
-  version: "0.6.7",
+  version: "0.6.8",
   git: "https://github.com/jackadams/meteor-editable-text.git"
 });
 
