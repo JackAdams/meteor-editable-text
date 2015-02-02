8a1b29f833a15ab05938a745c761591b0d3ebb99
Brent Abrahams <brent_abrahams@yahoo.com>
2015-02-02 09:26:50 +0800

    Fixes #10 bug with EditableText.useMethods = false

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:editable-text",
   summary: "Editable text drop-in template helper",
-  version: "0.7.7",
+  version: "0.7.8",
   git: "https://github.com/jackadams/meteor-editable-text.git"
 });
 
