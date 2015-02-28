86d5fb891e7a40f19c34e8c41da549ccba077911
Brent Abrahams <brent_abrahams@yahoo.com>
2015-03-01 03:47:27 +0800

    Auto-resizeable input fields

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:editable-text",
   summary: "Editable text drop-in template helper",
-  version: "0.7.12",
+  version: "0.7.13",
   git: "https://github.com/jackadams/meteor-editable-text.git"
 });
 
