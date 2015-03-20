20ce2b6debe54e463fa591bc4175f91da49bf3d9
Brent Abrahams <brent_abrahams@yahoo.com>
2015-03-21 07:32:07 +0800

    Fixes #18

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:editable-text",
   summary: "Editable text drop-in template helper",
-  version: "0.7.17",
+  version: "0.7.18",
   git: "https://github.com/jackadams/meteor-editable-text.git"
 });
 
